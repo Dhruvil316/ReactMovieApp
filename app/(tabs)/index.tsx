de81322c76fd529a5eff038fe9e84c19b8c07a60
Dhruvil314 <dhruvil.developer@gmail.com>
2025-05-19 12:58:50 +0530

    everthing is running -first commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,24 @@
-import { View, Image, ScrollView } from "react-native";
+import { View, Image, ScrollView, ActivityIndicator, Text } from "react-native";
 import { images } from "@/constants/images";
 import { icons } from "@/constants/icons";
 import SearchBar from "@/components/SearchBar";
 import { useRouter } from "expo-router";
+import useFetch from "@/services/useFetch";
+import { fetchPopularMovies } from "@/services/api";
 
 export default function Index() {
   const router = useRouter();
+
+  const {
+    data: movies,
+    loading: moviesLoading,
+    error: moviesError,
+  } = useFetch(() =>
+    fetchPopularMovies({
+      query: "",
+    })
+  );
+
   return (
     <View className="flex-1 bg-primary">
       <Image
@@ -24,12 +37,28 @@ export default function Index() {
         mx-auto"
         />
 
-        <View className="flex-1 mt-5">
-          <SearchBar
-            onPress={() => router.push("/search")}
-            placeholder="search for movie"
+        {moviesLoading ? (
+          <ActivityIndicator
+            size="large"
+            color="#0000ff"
+            className="mt-10 self-center"
           />
-        </View>
+        ) : moviesError ? (
+          <Text>Error: {moviesError?.message}</Text>
+        ) : (
+          <View className="flex-1 mt-5">
+            <SearchBar
+              onPress={() => router.push("/search")}
+              placeholder="search for movie"
+            />
+
+            <>
+              <Text className="text-lg  text-white font-bold mt-5 mb-3">
+                Latest movies
+              </Text>
+            </>
+          </View>
+        )}
       </ScrollView>
     </View>
   );
